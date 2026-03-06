1326ba575946486a14a27f0205865b5f6516ad24
Kurt Schwind <kurt.schwind@gmail.com>
2026-03-06 08:21:54 -0600

    Wiring buttons

--- starter-godot-apps/Calculator/scripts/calculator.gd
+++ starter-godot-apps/Calculator/scripts/calculator.gd
@@ -1,17 +1,21 @@
 extends Control
 
+@onready var label = $VBoxContainer/MarginContainer/ResultLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	label.text = ""
+	for button in $VBoxContainer/PanelContainer/GridContainer.get_children():
+		if button is Button:
+			button.connect('pressed', button_pressed.bind(button.text))
+	%ZeroButton.connect('pressed', button_pressed.bind("0"))
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
+func button_pressed(text: String):
+	print("button pressed %s" % text)
 
 func _on_zero_button_resized() -> void:
 	@warning_ignore("integer_division")
 	%ZeroButton.offset_left = -get_window().size.x / 4
-
